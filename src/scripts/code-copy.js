5ec26b5fceb3e14ccf8e0a0e7aa9f84d9111df27
Khôi Tran <tran@puzzle.ch>
2018-11-20 11:11:32 +0100

    Index generation

--- src/scripts/code-copy.js
+++ src/scripts/code-copy.js
@@ -1,15 +1,19 @@
 Reveal.addEventListener( 'ready', ev => {
 	document.querySelectorAll('code').forEach(el => {
     el.title = 'Click to copy!'
-    console.log('Adding click to ', el)
     el.addEventListener('click', ev => {
       document.oncopy = copyEvent => {
-        console.log('copied to clipboard')
         copyEvent.clipboardData.setData('Text', el.innerText)
         copyEvent.preventDefault()
       }
       document.execCommand('copy')
       document.oncopy = undefined
+
+      let elem = document.createElement('div');
+      elem.style.cssText = 'position:fixed; top: 20px; right: 20px; padding: 1em; opacity:0.8; z-index:100; background: #8df; border-radius: 5px; animation: fadeout 600ms; animation-delay: 1s'
+      elem.innerText = 'Code copied to clipboard!'
+      document.body.appendChild(elem);
+      setTimeout(() => document.body.removeChild(elem), 1600)
     })
   })
 })
